712bd272d763392b0db4abf6a0f4a40594537fa2
YASEENKHANO <myaseen.khan9498360@gmail.com>
2024-06-12 20:52:14 +0500

    Add project files.

---
+++ Gridviewcheck/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Gridviewcheck.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Testing data fetching from databsae to Gridview</h3>
+        </div>
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID"  />
+                    
+                    <asp:BoundField DataField="Name" HeaderText="Name"  />
+                    
+                    <asp:BoundField DataField="Price" HeaderText="Price"  />
+                </Columns>
+
+
+                <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+
+
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Gridviewcheck/WebForm1.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Gridviewcheck
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {//Check if the page is being loaded for the first time
+            if(!IsPostBack)
+            {//Call the method to bind data to the GridView
+                BindGrid();
+            }
+
+        }
+
+        //Method to bind the data to gridview
+        private void BindGrid()
+        {
+
+            //Define the SQL Query to select data from the Products table
+            string query = "Select Id, Name, Price From Products";
+
+            //Get the connection sting from the Web.config file
+            string connstring = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+
+            //Using statement ensures the connection is closed and disposed propeerly
+           using( SqlConnection conn= new SqlConnection(connstring))
+            {
+                //Create a sQlCommand object  to execute the query
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                //Open the SQL connection
+                conn.Open();
+
+                //Create a SQlDataAdapter to fill a DataTable with the result of thequery
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+                //Create a Datatable to hold the data
+                DataTable dt = new DataTable();
+
+                //Fill the DataTable with data from the database
+                sda.Fill(dt);
+
+                //Set the data source of thee GridView to the DataTable
+                GridView1.DataSource = dt;
+
+                //Bind the data to the Gridview
+                GridView1.DataBind();
+
+            }
+
+
+        }
+    }
+}
